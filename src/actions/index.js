de1cd2166975fe967fc7ae23735bcd8688b5c16d
VodkaSodaBrah <mchildress@me.com>
2022-02-28 13:35:59 -0500

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { SET_RECENT_POSTS } from "./types";
+import { 
+  SET_RECENT_POSTS,
+  SET_RESULTS_POSTS
+ } from "./types";
 
 import axios from "axios";
 
@@ -8,6 +11,18 @@ export function fetchRecentPosts() {
       console.log(response.data.posts);
       dispatch({
         type: SET_RECENT_POSTS,
+        payload: response.data.posts,
+      });
+    });
+  };
+}
+
+export function fetchPostWithQuery(query) {
+  return function (dispatch) {
+    axios.get(`https://api.dailysmarty.com/search?q=${query}`).then((response) => {
+      console.log(response.data.posts);
+      dispatch({
+        type: SET_RESULTS_POSTS,
         payload: response.data.posts,
       });
     });
---
+++ src/components/resultsPosts.js
@@ -0,0 +1,17 @@
+import React, { Component, useLayoutEffect } from 'react';
+
+class Results extends Component {
+    render () {
+        return (
+            <div className='results-posts'>
+                <div className='results-posts-wrapper'>
+                    <ul className='results-posts__posts'>
+                        results go here
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ResultsPosts;
